3ede3aa235ab27256086f3e02c7094c7a9d1c2e3
Toshi7031 <7031.toshi@gmail.com>
2020-12-11 11:49:18 +0900

    function 結びつけ

--- admin/functions/index_function.php
+++ admin/functions/index_function.php
@@ -1,36 +1,43 @@
 <?php
-function load_page_source() {
-// 画面の固定値を戻り値に指定
-    $const = [
-        "title" => "TOP | HALMORTOR",
-        "path_to_logo_image" => "./img/HALMOTOR.png",
-        "regist" => "登録",
-        "matter" => "案件",
-        "change" => "変更・管理",
-        "log_out" => "ログアウト",
-    ];
-    return $const;
-}
+require("../../vendor/autoload.php");
+require("../../config.php");
+require("./Login.php");
+require("./Modify.php");
+require("./Search.php");
+require("./SiginUp.php")
 
-function get_check_sample($name, $age, $gender, $address) {
+class AdminMain{
+    function load_page_source() {
+    // 画面の固定値を戻り値に指定
+        $const = [
+            "title" => "TOP | HALMORTOR",
+            "path_to_logo_image" => "./img/HALMOTOR.png",
+            "regist" => "登録",
+            "matter" => "案件",
+            "change" => "変更・管理",
+            "log_out" => "ログアウト",
+        ];
+        return $const;
+    }
+    function get_check_sample($name, $age, $gender, $address) {
 
-    $hoge = [
-        "ok" => "GETデータは取得できています。",
-        "name" => $name,
-        "age" => $age,
-        "gender" => $gender,
-        "address" => $address,
-    ];
-    return $hoge;
-}
-
-function post_check_sample($hobby, $qualification, $advantages, $disadvantages) {
-    $fuga = [
-        "ok" => "GETデータは取得できています。",
-        "hobby" => $hobby,
-        "qualification" => $qualification,
-        "advantages" => $advantages,
-        "disadvantages" => $disadvantages,
-    ];
-    return $fuga;
+        $hoge = [
+            "ok" => "GETデータは取得できています。",
+            "name" => $name,
+            "age" => $age,
+            "gender" => $gender,
+            "address" => $address,
+        ];
+        return $hoge;
+    }
+    function post_check_sample($hobby, $qualification, $advantages, $disadvantages) {
+        $fuga = [
+            "ok" => "GETデータは取得できています。",
+            "hobby" => $hobby,
+            "qualification" => $qualification,
+            "advantages" => $advantages,
+            "disadvantages" => $disadvantages,
+        ];
+        return $fuga;
+    }
 }
