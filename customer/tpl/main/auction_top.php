de902d7a96f382b014f61c8a5c261007d1615722
Kiharaten <sukerock324@gmail.com>
2021-01-12 16:46:36 +0900

    変更の反映

--- customer/tpl/main/auction_top.php
+++ customer/tpl/main/auction_top.php
@@ -1,7 +1,7 @@
 <main>
     <h2>現在開催中のオークション</h2>
-    <ul>
     <h2><a href="http://127.0.0.1/IH31/customer/index.php"><?= $auction_name ?></a></h2>
+    <ul>
         <?php foreach($cars as $column): ?>
             <li><?= $column ?></li>
         <?php endforeach ?>
