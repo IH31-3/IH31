fcfdf50690021a2753464cd3bde7b26d1706b415
hiraken0817 <58155849+hiraken0817@users.noreply.github.com>
2021-01-15 17:22:23 +0900

    変更の追加

--- customer/tpl/main/history_top.php
+++ customer/tpl/main/history_top.php
@@ -1,4 +1,65 @@
-<main>
-    <a href="http://127.0.0.1/IH31/customer/index.php?page=index_top">トップへ戻る</a>
+<main id="history_top">
+    <div id="main_image">
+    <img src="./img/mainVisual.png">
+    </div>
+
+    <article>
+        <h2><img src="./img/HALMOTOR.png" width="280px">とは</h2>
+        <p>大阪で展開している中古車販売企業です。<br>
+            土曜日に当サイトでオークションを行っています！<br>
+            契約に必要な書類もお車も、すべてご自宅にお届けします！<br>
+            とっても便利な中古車オークションサイトです。</p>
+        <section>
+            <p><img src=""></p>
+            <h3>ネットで入札</h3>
+            <p>簡単・スピーディ！
+                どこにいても入札可能！</p>
+        </section>
+        <section>
+            <p><img src=""></p>
+            <h3>ネットで取引情報チェック</h3>
+            <p>ネットで注文状況チェック！
+                いつでも安心！
+            </p>
+        </section>
+        <section>
+            <p><img src=""></p>
+            <h3>自宅に届く</h3>
+            <p>必要な書類からお車まで、
+                すべてご自宅にお届けします！</p>
+        </section>
+
+        <p><a href="">オークションに参加する</a></p>
+        <article>
+
+        <article>
+        <h2><span class="pink_back">THIS WEEK</span>AUCTION LINEUP <span class="pink_font"></span></h2>
+
+        <section class="car_sumnail">
+            <p><img src=""></p>
+            <h3>(メーカー)(車種名)</h3>
+            <dl>
+                <dt>年式</dt>
+                <dd>(年式)</dd>
+                <dt>走行量</dt>
+                <dd>(走行量)</dd>
+                <dt>排気量</dt>
+                <dd>(排気量)</dd>
+                <dt>車検</dt>
+                <dd>(車検)</dd>
+                <dt>修復歴</dt>
+                <dd>(修復歴)</dd>
+            </dl>
+            <dl>
+                <dt>開始価格</dt>
+                <dd>(開始価格価格)万円</dd>
+                <dt>オークション予定日時</dt>
+                <dd>(開始日時)〜(開始日時+10分)</dd>
+            </dl>
+        </section>
+
+        </article>
+
+        <a href="http://127.0.0.1/IH31/customer/index.php?page=index_top">トップへ戻る</a>
 </main>
 <!-- main ここまで -->
