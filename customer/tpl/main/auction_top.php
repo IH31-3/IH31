608b8025d1fb8bbe2fe69ada1941c79ed3ceec90
Sora Kihara <sorakihara@SoranoiMac.local>
2021-01-16 21:33:20 +0900

    オークション画面にヘッダーフッター配置

--- customer/tpl/main/auction_top.php
+++ customer/tpl/main/auction_top.php
@@ -35,19 +35,19 @@
             </dl>
         </section>
 
-        <p class="enter_button pink"><a href="">この車両に入札する</a></p><!--開催中-->
+        <p class="enter_button pink"><a href="<?= $auction_bid_url ?>">この車両に入札する</a></p><!--開催中-->
         <p class="enter_button">この車両に入札する</p><!--開催中以外-->
 
 
     </article>
 
 
-    <h2><a href="http://127.0.0.1/IH31/customer/index.php"><?= $auction_name ?></a></h2>
+    <!-- <h2><a href="http://127.0.0.1/IH31/customer/index.php"><?= $auction_name ?></a></h2>
     <ul>
         <?php foreach ($cars as $column) : ?>
             <li><?= $column ?></li>
         <?php endforeach ?>
-    </ul>
+    </ul> -->
 
     <a href="http://127.0.0.1/IH31/customer/index.php">トップへ戻る</a>
 </main>
