33626480800e4ff7e723d9fa42f23ad63057a762
Toshi7031 <7031.toshi@gmail.com>
2021-01-17 18:23:12 +0900

    不要部分削除

--- defaultConfig.php
+++ defaultConfig.php
@@ -1,11 +1,4 @@
 <?php
-$HOSTNAME = "localhost";
-$MYSQL_HOST = "localhost:3306";
-$MYSQL_USER = "root";
-$MYSQL_PASS = "root";
-$NODE_PORT = 9000;
-
-
 const HOSTNAME = "localhost";
 const MYSQL_HOST = "localhost:3306";
 const MYSQL_USER = "root";
