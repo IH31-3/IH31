ab3a51273339ce48920d3a687666501c4514161c
Kiharaten <sukerock324@gmail.com>
2021-01-17 17:37:04 +0900

    DB

--- defaultConfig.php
+++ defaultConfig.php
@@ -4,3 +4,10 @@ $MYSQL_HOST = "localhost:3306";
 $MYSQL_USER = "root";
 $MYSQL_PASS = "root";
 $NODE_PORT = 9000;
+
+
+const HOSTNAME = "localhost";
+const MYSQL_HOST = "localhost:3306";
+const MYSQL_USER = "root";
+const MYSQL_PASS = "";
+const DB_NAME = "vehicle_auction_site_db";
