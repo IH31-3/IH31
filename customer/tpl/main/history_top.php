650f14dd62c97c3a839b51a90f397db266a87d1b
hiraken0817 <58155849+hiraken0817@users.noreply.github.com>
2021-01-18 16:31:35 +0900

    css変更

--- customer/tpl/main/history_top.php
+++ customer/tpl/main/history_top.php
@@ -1,39 +1,53 @@
 <main id="history_top">
     <div id="main_image">
-    <img src="./img/mainVisual.png">
+        <img src="./img/mainVisual.png">
     </div>
 
     <article>
-        <h2><img src="./img/HALMOTOR.png" width="280px">　とは</h2>
+        <p class="center bold bottom30"><img src="./img/HALMOTOR.png" width="280px">　とは</p>
         <p class="center lheight">大阪で展開している中古車販売企業です。<br>
             土曜日に当サイトでオークションを行っています！<br>
             契約に必要な書類もお車も、すべてご自宅にお届けします！<br>
             とっても便利な中古車オークションサイトです。</p>
-        <section>
-            <p><img src="./img/ex1.png"></p>
-            <h3>ネットで入札</h3>
-            <p>簡単・スピーディ！
-                どこにいても入札可能！</p>
-        </section>
-        <section>
-            <p><img src="./img/ex2.png"></p>
-            <h3>ネットで取引情報チェック</h3>
-            <p>ネットで注文状況チェック！
-                いつでも安心！
-            </p>
-        </section>
-        <section>
-            <p><img src="./img/ex3.png"></p>
-            <h3>自宅に届く</h3>
-            <p>必要な書類からお車まで、
-                すべてご自宅にお届けします！</p>
-        </section>
+        <div id="contentsA">
+            <section class="contA">
+                <dl>
+                    <dt class="back_pink">STEP 01</dt>
+                    <dd><img src="./img/ex1.png"></dd>
+                    <dd class="font_blue">ネットで入札</dd>
+                    <dd>簡単・スピーディ！<br>
+                        どこにいても入札可能！</dd>
+                </dl>
+            </section>
 
-        <p><a href="">オークションに参加する</a></p>
-        <article>
+            <section class="contA">
+                <dl>
+                    <dt class="back_pink">STEP 02</dt>
+                    <dd><img src="./img/ex2.png"></dd>
+                    <dd class="font_blue">ネットで取引情報チェック</dd>
+                    <dd>ネットで注文状況チェック！<br>
+                        いつでも安心！</dd>
+                </dl>
+            </section>
 
-        <article>
-        <h2><span class="pink_back">THIS WEEK</span>AUCTION LINEUP <span class="pink_font"></span></h2>
+            <section class="contA">
+                <dl>
+                    <dt class="back_pink">STEP 03</dt>
+                    <dd><img src="./img/ex3.png"></dd>
+                    <dd class="font_blue">自宅に届く</dd>
+                    <dd>必要な書類からお車まで、<br>
+                        すべてご自宅にお届けします！</dd>
+                </dl>
+            </section>
+
+        </div>
+
+        <p class="out_line"><a class="btn1 view_center" href=""><span class="padding_img"><img src="./img/auction.png" width="20px"></span> オークションに参加する</a></p>
+    </article>
+
+    <article>
+        <h2 class="left border_bottom"><span class="back_pink">THIS WEEK</span>　　　<span class="font_gray font_size_middle">AUCTION LINEUP</span> <span class="pink_font"></span></h2>
+        
 
         <section class="car_sumnail">
             <p><img src=""></p>
@@ -58,8 +72,8 @@
             </dl>
         </section>
 
-        </article>
+    </article>
 
-        <a href="http://127.0.0.1/IH31/customer/index.php?page=index_top">トップへ戻る</a>
+    <a href="http://127.0.0.1/IH31/customer/index.php?page=index_top">トップへ戻る</a>
 </main>
 <!-- main ここまで -->
