2893d4ad299f5ceb24af1126c88fd4344a1f9b5a
Toshi7031 <7031.toshi@gmail.com>
2021-01-19 01:15:36 +0900

    案件検索

--- functions/archived/select.php
+++ functions/archived/select.php
@@ -8,26 +8,45 @@ function search_matter(){
 
    //初回アクセス時には指定なしでの検索
    if(!empty($_GET['status'])){
+      //ステータス検索
       $status = $_GET['status'];
       $sql = "SELECT matter.matter_no, matter.status,client.client_name,employee.employee_name,matter.vehicle_no,matter.money FROM matter JOIN client ON matter.client_no = client.client_no JOIN employee ON employee.employee_no = matter.employee_no WHERE status = ?";
-
-      $stmt = $cn -> prepare($sql);
-      $stmt -> bind_param("s",$status);
+      //ワード検索
+      if(!empty($_GET['text'])){
+         $text = "%".$_GET['text']."%";
+         $sql .= "AND employee.employee_name LIKE ?";
+         $stmt = $cn -> prepare($sql);
+         $stmt -> bind_param("ss",$status,$text);
+      }
+      else{
+         $stmt = $cn -> prepare($sql);
+         $stmt -> bind_param("s",$status);
+      }
    }
    else{
+      //ステータス検索指定なし
       $sql = "SELECT matter.matter_no, matter.status,client.client_name,employee.employee_name,matter.vehicle_no,matter.money FROM matter JOIN client ON matter.client_no = client.client_no JOIN employee ON employee.employee_no = matter.employee_no";
-      $stmt = $cn -> prepare($sql);
+      //ワード検索
+      if(!empty($_GET['text'])){
+         $text = "%".$_GET['text']."%";
+         $sql .= " WHERE employee.employee_name LIKE ?";
+         $stmt = $cn -> prepare($sql);
+         $stmt -> bind_param("s",$text);
+      }
+      else{
+         $stmt = $cn -> prepare($sql);
+      }
    }
    $stmt -> execute();
    $stmt -> bind_result($matter_no,$status,$client_name,$employee_name,$vehicle_no,$money);
    for($i = 0;$stmt -> fetch();$i++){
       $result[$i] = array(
-         $matter_no,
-         $status,
-         $client_name,
-         $employee_name,
-         $vehicle_no,
-         $money
+         'matter_no' => $matter_no,
+         'status' => $status,
+         'client_name' => $client_name,
+         'employee_name' => $employee_name,
+         'vehicle_no' => $vehicle_no,
+         'money' => $money
       );
    }
    $cn -> close();
